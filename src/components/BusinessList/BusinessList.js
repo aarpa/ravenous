046628138876d4e61869786b0257685c8c18aa89
Arpa Imran <aarpa128@gmail.com>
2018-10-22 16:05:31 -0700

    Passed info from parent component (App) to child components (BusinessList and Business).

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,12 +6,20 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business/>
-        <Business/>
-        <Business/>
-        <Business/>
-        <Business/>
-        <Business/>
+        // Create an array of these businesses in App.js
+        // <Business/>
+        // <Business/>
+        // <Business/>
+        // <Business/>
+        // <Business/>
+        // <Business/>
+
+        // Access businesses prop and call the .map() method on it
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business}/>; // Creates a <Business/> component, and adds a 'busines' prop, and sets value to parameter of callback function
+          })
+        }
       </div>
     );
   }
